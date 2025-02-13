0df8546f66f95937b474b032d612f6053a9e12af
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2025-02-13 14:00:04 +0700

    Second commit

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/rest_go_db")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/migration_go_db")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -16,4 +16,18 @@ func NewDB() *sql.DB {
 	db.SetConnMaxIdleTime(10 * time.Minute)
 
 	return db
+
+	// create migrate
+	// migrate create -ext sql -dir db/migrations create_table_first
+	// migrate create -ext sql -dir db/migrations create_table_second
+	// migrate create -ext sql -dir db/migrations create_table_third
+	// migrate create -ext sql -dir db/migrations sample_dirty_state
+
+	// run migrate
+	// migrate -database 'mysql://root:root@tcp(localhost:3306)/migration_go_db' -path db/migrations up
+	// migrate -database 'mysql://root:root@tcp(localhost:3306)/migration_go_db' -path db/migrations down
+
+	// force rollback
+	// migrate -database 'mysql://root:root@tcp(localhost:3306)/migration_go_db' -path db/migrations version
+	// migrate -database 'mysql://root:root@tcp(localhost:3306)/migration_go_db' -path db/migrations force 20250213063547
 }
